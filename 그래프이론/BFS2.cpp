5e6056b8cc8fdb732d88c30fd406543370b74dcf
hhongyeahh <hhongyeahh@gmail.com>
2025-02-07 20:53:01 +0900

    [feat] BFS #1

---
+++ 그래프이론/BFS2.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> adj[100];
+int visited[100];
+int nodeList[] = {10, 12, 14, 16, 18, 20, 22, 24};
+
+void BFS(vector<int> startNodes) {
+    queue<int> q;
+
+    // 여러 개의 시작점 처리
+    for (int start : startNodes) {
+        visited[start] = 1; // 시작점 거리 = 1 (0이 아니라 1로 설정)
+        q.push(start);
+    }
+
+    while (q.size()) {
+        int here = q.front();
+        q.pop();
+        for (int there : adj[here]) {
+            if (visited[there]) continue;
+            visited[there] = visited[here] + 1;
+            q.push(there);
+        }
+    }
+}
+
+int main() {
+    adj[10].push_back(12);
+    adj[10].push_back(14);
+    adj[10].push_back(16);
+
+    adj[12].push_back(18);
+    adj[12].push_back(20);
+
+    adj[20].push_back(22);
+    adj[20].push_back(24);
+
+    // 1, 2, 3을 동시에 시작점으로 설정
+    vector<int> startNodes = {1, 2, 3};
+    BFS(startNodes);
+
+    // 출력
+    for (int i : nodeList) {
+        cout << i << " : " << visited[i] - 1 << "\n"; // 거리 조정
+    }
+
+    cout << "10번으로부터 24번까지 최단거리는 : " << visited[24] - 1 << '\n';
+    return 0;
+}
